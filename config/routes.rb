ba4fbfe4797f5f0ecfb10a42544f8416e63ada11
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-31 13:34:02 -0500

    setup sessions/user controllers/view folders + their routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "sessions#welcome"
+  get "signup" => "users#new"
+  post "signup" => "users#create"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
+
+  resources :users, only: [:show, :new, :create]
 end
