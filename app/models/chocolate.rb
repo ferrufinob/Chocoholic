c6aa86f5d203eeeea9a8789b0a64268b5adc10f9
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-31 09:57:36 -0500

    generated Chocolate model

---
+++ app/models/chocolate.rb
@@ -0,0 +1,4 @@
+class Chocolate < ApplicationRecord
+  belongs_to :user
+  belongs_to :category
+end
---
+++ db/migrate/20210131145453_create_chocolates.rb
@@ -0,0 +1,15 @@
+class CreateChocolates < ActiveRecord::Migration[6.0]
+  def change
+    create_table :chocolates do |t|
+      t.string :brand
+      t.string :flavor
+      t.string :country
+      t.string :note
+      t.string :cocoa
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
