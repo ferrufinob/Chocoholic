9b29fc1dcbb7c4c950fd44edba3ec5a8a63ada54
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-31 16:17:39 -0500

    created new user form/action & user signed up successful

--- app/models/chocolate.rb
+++ app/models/chocolate.rb
@@ -3,6 +3,8 @@ class Chocolate < ApplicationRecord
   belongs_to :user #that they created
   has_many :reviews
   has_many :reviewers, through: :reviews, source: :user #recieved
+
+  # ! leave this last, deal with after I meet all requirements
   # has_many :chocolate_tasting_terms
-  has_many :tasting_terms, through: :chocolate_tasting_terms
+  # has_many :tasting_terms, through: :chocolate_tasting_terms
 end
--- app/models/tasting_term.rb
+++ app/models/tasting_term.rb
@@ -1,4 +1,4 @@
 class TastingTerm < ApplicationRecord
-  has_many :chocolate_tasting_terms #my look up table
-  has_many :chocolates, through: :chocolate_tasting_terms #
+  has_many :chocolate_tasting_terms #terms
+  has_many :chocolates, through: :chocolate_tasting_terms #the chocolates with those terms
 end
