890aa3c8a54d5fe1721832ad3022e399447dc885
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-31 19:38:37 -0500

    added chocolates show/index page & added categories controller and routes

--- app/controllers/chocolates_controller.rb
+++ app/controllers/chocolates_controller.rb
@@ -1,5 +1,10 @@
 class ChocolatesController < ApplicationController
   def index
     @chocolates = Chocolate.all
+    @categories = Category.all
+  end
+
+  def new
+    @chocolate = Chocolate.new
   end
 end
