3e55cd688177fa74bae9f34f7bf1a1bad9eb0321
Brenda Ferrufino <ferrufinob@gmail.com>
2021-01-31 20:07:49 -0500

    can create a new chocolate with collection category/ need to add fields for category

--- app/controllers/chocolates_controller.rb
+++ app/controllers/chocolates_controller.rb
@@ -2,9 +2,36 @@ class ChocolatesController < ApplicationController
   def index
     @chocolates = Chocolate.all
     @categories = Category.all
+    # @chocolate.build_category
   end
 
   def new
     @chocolate = Chocolate.new
+    @categories = Category.all
+  end
+
+  def create
+    @chocolate = current_user.chocolates.build(chocolate_params)
+
+    binding.pry
+
+    if @chocolate.save
+      redirect_to chocolates_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def chocolate_params
+    params.require(:chocolate).permit(
+      :brand,
+      :flavor,
+      :cocoa,
+      :country,
+      :note,
+      :category_id
+    )
   end
 end
