03c2c6932e2b8a3bb67da481b4d2a36ed5151456
Brenda Ferrufino <ferrufinob@gmail.com>
2021-03-03 14:13:42 -0500

    minor style changes. margins/paddings

--- app/helpers/chocolates_helper.rb
+++ app/helpers/chocolates_helper.rb
@@ -2,4 +2,12 @@ module ChocolatesHelper
   def human_boolean(boolean)
     boolean ? "Yes" : "No"
   end
+
+  def count_display(reviews)
+    if reviews.count > 1 || reviews.count == 0
+      "#{reviews.count}" + " reviews"
+    else
+      "#{reviews.count}" + " review"
+    end
+  end
 end
